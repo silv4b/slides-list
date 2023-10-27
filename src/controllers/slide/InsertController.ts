5dc6fbd18fb4a2fd7283aeb05fdb7d2af1c2dae6
Bruno Silva <brunno.linkin@gmail.com>
2023-10-27 16:01:41 -0300

    adicionando controllers de remover e adicionar slides

---
+++ src/controllers/slide/InsertController.ts
@@ -0,0 +1,19 @@
+import supabase from "../../../lib/supabase-client";
+import { SlideType } from "../../../types/slide";
+
+// recebe a tabela e o objeto montado no formulário que serão usados para fazer o insert no supabase
+export const insertSlide = async (
+  tableName: string,
+  dataToInsert: SlideType
+) => {
+  const { data, error } = await supabase
+    .from(tableName) // busca a tabala destino
+    .insert([dataToInsert]) // faz o inserto dos dados
+    .select(); // returning do dado para exibição
+  if (error) {
+    return error;
+  } else {
+    // retorna a primeira posição do data, o qual eu posso acessar os dados do formato FormData
+    return data[0].title;
+  }
+};
---
+++ types/slide.ts
@@ -0,0 +1,7 @@
+export type SlideType = {
+  title: string;
+  subtitle: string;
+  url: string;
+}
+
+// TODO: Ver a abordagem recomendada para fazer esses tipos
